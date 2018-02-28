58397b9764b38851e20148d96b1445c5d55c322b
medardo <mbonillat@correo.udistrital.edu.co>
2018-02-28 18:08:27 -0500

    Celador irrespetuoso

---
+++ Parqueadero1/src/parqueadero1/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parqueadero1;
+
+/**
+ *
+ * @author DELL
+ */
+public class Principal {
+    public static void main (String [] args){
+        int nivelalchol;
+        String genero;
+ 
+        }
+      
+        
+        
+        
+        
+    }
+    
+}
